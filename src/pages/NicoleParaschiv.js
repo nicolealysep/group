1af925b1297e5e00bbe9be82858a0fb4240c5d5f
Nicole Paraschiv <143670702+nicolealysep@users.noreply.github.com>
2023-11-16 14:58:11 -0700

    Update NicoleParaschiv.js

--- src/pages/NicoleParaschiv.js
+++ src/pages/NicoleParaschiv.js
@@ -6,23 +6,18 @@ export default function examplePage() {
 
         <main>
           <div className='underline'>
-            <h1 className='title'>Hey, I'm Spider man. </h1>
+            <h1 className='title'>Hey I'm Nicole. </h1>
           </div>
           <img
             className='pic'
             src='https://i.postimg.cc/qR2VgDkd/spiderman.jpg'
-            alt='tom holland'
+            alt='nicole paraschiv'
           />
 
           <p className='description'>
-            Hey I am your friendly neighbourhood Spider Man. My real name is Tom
-            Holland. I am an actor but mostly I fight villains and juggle high school.
-            I am just your average student – well, maybe not so average. 
-            You see, I've got some incredible powers, like superhuman strength, agility, 
-            and the ability to shoot webs from my wrists. With great power comes great 
-            responsibility, and I'm here to protect New York City from all kinds of baddies,
-            from common crooks to supervillains. So, if you ever need a hand or a web-swinging
-            hero in your corner, just remember, I'm your friendly neighborhood Spider-Man!"
+            Hi my name is Nicole! I am a student at CU Boulder studying computer science!
+            I am from Washington State and I love it there, Colorado isn't as great 🤣
+            My favorite hobby is yoga but I also love watching action movies. 
           </p>
 
           <a className='button' href='/'>

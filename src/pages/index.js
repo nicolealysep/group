f447085719a5f5755c60f7a1bf43e042864e6327
Ella Larson <143456994+ellalarson@users.noreply.github.com>
2023-11-16 14:48:21 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,11 +24,7 @@ export default function Home() {
         <code>src/page/teamMemberName.js</code>
 
         <div className='grid'>
-          <a href='/TomHolland ' className='card'>
-            <h3>Example About Me Page &rarr;</h3>
-            <p>Click here to navigate to an example page</p>
-          </a>
-
+      
           /////////////////addison
           <a href='/AddisonGetz ' className='card'>
             <h3>Addison Getz &rarr;</h3>
